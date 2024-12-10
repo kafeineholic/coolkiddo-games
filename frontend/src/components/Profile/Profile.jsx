fb8cc5c0f9a470ffb153ad9db48c1f683026fc0a
kafeineholic <mind.pattranith@gmail.com>
2024-12-10 09:56:48 +0700

    Update Profile.jsx

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -48,7 +48,7 @@ const Profile = () => {
                 id="disabled-input"
                 aria-label="disabled input"
                 className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={userData.username}
+                value={userData.email}
                 disabled={true}
             />
 
@@ -57,7 +57,7 @@ const Profile = () => {
                 id="disabled-input"
                 aria-label="disabled input"
                 className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={userData.username}
+                value={userData.full_name}
                 disabled={true}
             />
             <input
@@ -65,7 +65,7 @@ const Profile = () => {
                 id="disabled-input"
                 aria-label="disabled input"
                 className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={userData.username}
+                value={userData.phone}
                 disabled={true}
             />
         </form>
