80e90b623b55c285b63ae0a3fa286fb950297307
lbib <rhitakane@gmail.com>
2024-12-11 01:42:23 +0700

    Tic-Tac-Toe Uploaded

--- frontend/src/components/Games/PacMan.jsx
+++ frontend/src/components/Games/PacMan.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
 function PacMan() {
-  return (
-      <div style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%' ,
-          backgroundColor: '#0a0743'
-      }}>
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            backgroundColor: '#0a0743'
+        }}>
 
-          <iframe
-              src="/pacman/pacman.html" // Adjust the path if necessary
-              width="431"
-              height="700"
-              title="PACMAN Game"
-              style={{ border: 'none' }}
-          />
-      </div>
-  );
+            <iframe
+                src="/pacman/pacman.html" // Adjust the path if necessary
+                width="431"
+                height="700"
+                title="PACMAN Game"
+                style={{ border: 'none' }}
+            />
+        </div>
+    );
 }
 
 export default PacMan;
--- frontend/src/components/Games/TicTacToe.jsx
+++ frontend/src/components/Games/TicTacToe.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const TicTacToe = () => {
+function TicTacToe() {
   return (
-    <div>tictactoe</div>
-  )
+      <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+      }}>
+
+          <iframe
+              src="/tic-tac-toe/tictactoe.html" // Adjust the path if necessary
+              width="100%"
+              height="768px"
+              title="Tic-Tac-Toe Game"
+              style={{ border: 'none' }}
+          />
+      </div>
+  );
 }
 
 export default TicTacToe;
