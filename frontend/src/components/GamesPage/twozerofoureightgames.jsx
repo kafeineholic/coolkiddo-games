1f26208601c17ba9c44db5942f3c65c735871edd
kafeineholic <mind.pattranith@gmail.com>
2024-12-10 10:29:05 +0700

    Update twozerofoureightgames.jsx

--- frontend/src/components/GamesPage/twozerofoureightgames.jsx
+++ frontend/src/components/GamesPage/twozerofoureightgames.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const twozerofoureightgames = () => {
   return (
-    <div>twozerofoureightgames</div>
+    <>
+      <div id="game-container">
+        <div id="grid-container" />
+      </div>
+      <div id="game-over">
+        <h1>Game Over</h1>
+        <button id="tryagain">Try Again</button>
+      </div>
+      <div className="game-info">
+        <h1>2048 Game</h1>
+        <div id="game-message" className="game-message">
+          <p id="game-time">Time: 00:00</p>
+          <p id="game-score">Score: 0</p>
+        </div>
+        <button id="new-game">New Game</button>
+        <button id="pause-resume">Pause</button>
+      </div>
+    </>
   )
 }
 
