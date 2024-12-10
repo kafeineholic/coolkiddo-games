b8c45f45707462f823b6328cb359f2f052e49646
lbib <rhitakane@gmail.com>
2024-12-11 01:38:14 +0700

    PACMAN updated

--- frontend/src/components/Games/PacMan.jsx
+++ frontend/src/components/Games/PacMan.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const PacMan = () => {
+function PacMan() {
   return (
-    <div>pacman</div>
-  )
+      <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%' ,
+          backgroundColor: '#0a0743'
+      }}>
+
+          <iframe
+              src="/pacman/pacman.html" // Adjust the path if necessary
+              width="431"
+              height="700"
+              title="PACMAN Game"
+              style={{ border: 'none' }}
+          />
+      </div>
+  );
 }
 
 export default PacMan;
