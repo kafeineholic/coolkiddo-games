0655d9210b8da101377756b0cc225589b80810f6
kafeineholic <mind.pattranith@gmail.com>
2024-12-10 16:14:47 +0700

    Update 2.jsx

--- frontend/src/components/TrendingGames/2.jsx
+++ frontend/src/components/TrendingGames/2.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 const TwoZeroFourEightGames = () => {
   return (
     <div>TwoZeroFourEightGames</div>
-  )
-}
+  );
+};
 
 export default TwoZeroFourEightGames;
