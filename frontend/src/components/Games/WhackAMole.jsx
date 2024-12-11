810e95bbe709b36e89c9c82c6edf00047a3c743e
pichchanya-m <aimmy007@gmail.com>
2024-12-11 14:40:23 +0700

    nav

--- frontend/src/components/Games/WhackAMole.jsx
+++ frontend/src/components/Games/WhackAMole.jsx
@@ -1,23 +1,47 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
+import { assets } from '../../assets/assets'; 
+import { useNavigate } from 'react-router-dom'; 
+
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="w-full flex justify-between items-center py-2 px-4 sm:py-3 sm:px-16 absolute top-0 bg-[#40826d] bg-opacity-75 z-10">
+      <img
+        onClick={() => navigate('/')}
+        src={assets.game} 
+        alt="Game Logo"
+        className="w-8 h-8 sm:w-12 sm:h-12 cursor-pointer" 
+      />
+      <h1 className="text-xl sm:text-2xl text-white font-bold">Whack A Mole</h1>
+    </div>
+  );
+};
 
 function WhackAMole() {
-    return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-        }}>
-  
-            <iframe
-                src="/whackamole/whackamole.html" // Adjust the path if necessary
-                width="500"
-                height="700"
-                title="Tic-Tac-Toe Game"
-                style={{ border: 'none' }}
-            />
-        </div>
-    );
-  }
-  
-  export default WhackAMole;
+  return (
+    
+    <div className="relative">
+      <Navbar />
+      
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+       
+      }}>
+        <iframe
+          src="/whackamole/whackamole.html" 
+          width="500"
+          height="700"
+          title="Whack A Mole Game"
+          style={{ border: 'none' }}
+        />
+      </div>
+     
+    </div>
+  );
+}
+
+export default WhackAMole;
