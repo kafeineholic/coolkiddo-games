1670c4d56618a780e9073f20e71850781aa7f42f
lbib <rhitakane@gmail.com>
2024-12-11 17:33:50 +0700

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,9 +4,11 @@ import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg
 
 const Footer = () => {
     return (
-        <footer className="relative text-white bg-[#40826d] pt-20">
+        <footer className="relative text-white bg-[#40826d] pt-20"
+        >
             {/* Decorative SVG */}
             <div className="absolute top-0 left-0 w-full overflow-hidden">
+                
                 <svg
                     data-name="Layer 1"
                     xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +17,8 @@ const Footer = () => {
                 >
                     <path
                         d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                        className="relative block fill-white"
+                        className="relative block"
+                        style={{ fill: 'rgba(255, 255, 255)' }} 
                     ></path>
                 </svg>
             </div>
@@ -84,7 +87,7 @@ const Footer = () => {
 
             {/* Footer Bottom */}
             <div className="bg-[#366a56] text-center py-4">
-                <p className="text-sm">© 2024 Your Company Name. All rights reserved.</p>
+                <p className="text-sm">© 2024 Dodo Inc. All rights reserved.</p>
             </div>
         </footer>
     );
