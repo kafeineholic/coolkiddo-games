d6eb4ebe09bde5ada2a3672f49ba266095ce4f05
kafeineholic <mind.pattranith@gmail.com>
2024-12-12 16:10:01 +0700

    up

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,27 +4,31 @@ import { AppContext } from '../context/AppContext'
 
 const Header = () => {
 
-const {userData} = useContext(AppContext)
+   const { userData } = useContext(AppContext)
 
-  return (
-    <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
-        <img src={assets.header_img} alt=""
-         className='w-36 h-36 rounded-full mb-6'/>
+   return (
+      <div className='flex flex-col items-center mt-20 px-4 text-center text-[#04361D]'>
+         <img src={assets.header_img} alt=""
+            className='w-36 h-36 rounded-full mb-6' />
 
          <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>
             Hey {userData ? userData.name : 'Gamer'}!
-            <img src={assets.hand_wave} alt="" 
-         className='w-8 aspect-square'/></h1>
+            <img src={assets.hand_wave} alt=""
+               className='w-8 aspect-square' /></h1>
 
          <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>
             Welcome to our website</h2>
          <p className='mb-8 max-w-md'>
-            Let's start with a quick game blah blah blah blah blah blah</p>
-        <button className='border border-gray-500 rounded-full px-8 py-2.5
-        hover:bg-gray-100 transition-all '>
-            Get Started</button>
-    </div>
-  )
+            Let's start with a quick game</p>
+         <button className='btn shadow-[0_4px_6px_rgba(255,255,255,0.3)] hover:shadow-[0_6px_8px_rgba(255,255,255,0.5)] 
+           text-black bg-white ease-out hover:translate-y-1 transition-all 
+           rounded-full px-8 py-3 text-lg font-semibold hover:bg-gray-100'>
+            Get Started
+         </button>
+
+
+      </div>
+   )
 }
 
 export default Header
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Header from '../components/Header'
 
+import webBg from '../assets/webbg.png';
+import Snowfall from 'react-snowfall'; // Import the snowfall library
+
+
 const Home = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen 
-    bg-white '>
-       <Navbar/>
-       <Header/>
+    <div className='flex flex-col items-center justify-center min-h-screen '
+      style={{
+        backgroundImage: `url(${webBg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width:'100%',
+        minHeight: '100vh',
+      }}
+    >
+      <Snowfall color="white" snowflakeCount={100} style={{ position: 'absolute', zIndex: 1 }} />
+      <Navbar />
+      <Header />
     </div>
   )
 }
