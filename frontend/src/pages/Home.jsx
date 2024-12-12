ca6b59d2a12c7a22b1486b12163fc654d095e243
pichchanya-m <aimmy007@gmail.com>
2024-12-12 19:35:47 +0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar';
 import Header from '../components/Header'
 import Leaderboard from './Leaderboard';
 import webBg from '../assets/webbg.png';
