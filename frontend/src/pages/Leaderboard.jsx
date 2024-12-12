ef0f3355c8cb1c43423ee70f5feaa41c186496d5
lbib <rhitakane@gmail.com>
2024-12-13 01:56:43 +0700

    Update Leaderboard.jsx

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -55,7 +55,7 @@ const Leaderboard = () => {
 
     return (
         <div className="leaderboard-container max-w-4xl mx-auto p-6 bg-blue-300 rounded-lg shadow-xl mt-5">
-            <h2 className="text-3xl font-bold text-center mb-6 text-black">Leaderboard</h2>
+            <h2 className="text-3xl font-bold text-center mb-6 text-white">Leaderboard</h2>
             <table className="table-auto w-full text-left text-black">
                 <thead>
                     <tr className="bg-blue-400">
